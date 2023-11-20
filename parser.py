337a2c4981177b8914dd4d432ead4cb369d8a258
oarsaw <oarsaw@gmail.com>
2023-11-21 02:47:56 +0300

    Исправила но работает 3 секунды только

--- parser.py
+++ parser.py
@@ -1,41 +1,37 @@
 import codecs
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import json
+from selenium.webdriver.common.by import By
 
-            #Не поняла как установить pip install selenium сработал а потом выдаёт ошибку ModuleNotFoundError: No module named 'selenium'
-            #можете кинуть все библиотеки которые надо скачать пожалуйста, спасибо!!!!!!!!
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-
 def main():
-    driver.get("https://www.wildberries.ru/")
+    driver.get('https://www.ozon.ru/')
 
-    for i in range(1, 100, 15):
-        driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight * {i} / 100);")
+    for i in range (1,100,25):
+        driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight * {i}/100);")
         time.sleep(1)
 
-    goods_arr = driver.find_elements(By.CLASS_NAME, "product-cardwrapper")
-    goods_json = []
+    goods_arr = driver.find_elements(By.CLASS_NAME, "a5h cd5a dac5")
+    goods_json=[]
 
     for good in goods_arr:
-        description = good.find_elements(By.CLASS_NAME, 'product-cardname')[0].text
-        price = good.find_elements(By.CLASS_NAME, 'pricelower-price')[0].text
-        href = good.find_elements(By.CLASS_NAME, 'product-cardlink')[0].get_attribute('href')
+        desc = good.find_elements(By.CLASS.NAME, 'tsBody500Medium')[0].text
+        price = good.find_elements(By.CLASS.NAME, 'c3118-a0')[0].text
+        href = good.find_elements(By.CLASS.NAME, '_blank')[0].get.attribute('href')
 
         good_json = {
-            'product': description,
-            'price': price,
-            'link': href
+
+            'product':desc,
+            'price':price,
+            'link':href
         }
-        goods_json.append(good_json)
+        goods_json.append((good_json))
 
-    with codecs.open("vvv.json", "wb", "utf=16") as stream:
-        json.dump(goods_json, stream, ensure_ascii=False)
-
-
+        with codecs.open("vvv.json","WB", "utf=16"):
+            json.dump(goods_json,stream,ensure_ascii=false)
 main()
